7b34fafa8b39c087e7dc1cbbbfb1e8314c2a3dfd
Saqib Mir <101538923+suqqie@users.noreply.github.com>
2025-05-22 17:15:20 +1000

    Products table removed for some reason... fixed now

--- ass1/src/main/java/model/dao/DB.java
+++
@@ -1,9 +0,0 @@
-package model.dao;
-
-import java.sql.Connection;
-
-public class DB {
-    protected String URL = "jdbc:sqlite:C:/IotBay/Iotbay.db";// identify the database url
-    protected String driver = "org.sqlite.JDBC"; 
-    protected Connection conn;
-}
