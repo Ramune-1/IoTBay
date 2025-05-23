3580beac2f957518f0c18d884caadfa684abfc53
GDTran15 <trangiaiduong1512@gmail.com>
2025-05-24 02:53:24 +1000

    staff done

---
+++ ass1/src/main/java/model/dao/DB.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.sql.Connection;
+
+public class DB {
+
+
+    protected String URL = "jdbc:sqlite:C:/IotBay/Iotbay.db";// identify the database url
+    protected String driver = "org.sqlite.JDBC"; 
+    protected Connection conn;
+
+}
+
